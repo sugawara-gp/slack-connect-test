78123392e43f8961aaa0a1edc70a53d450acea6e
daichi <sareknon@gmail.com>
2016-01-13 23:35:06 +0900

    add bodyParser and using POST method

--- index.js
+++ index.js
@@ -5,21 +5,23 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
+var PORT = process.env.PORT || 3000;
+var SLACK_TOKEN = process.env.SLACK_TOKEN;
 
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.get('/hook', (req, res)=> {
+app.post('/hook', (req, res)=> {
     // TODO Validate Token
-    io.emit('ding', 'ding-message');
-    res.send(200);
+    if (req.body.token == SLACK_TOKEN){
+        io.emit('ding', 'ding-message');
+        res.send(200);
+    }else{
+        res.send(404);
+    }
 });
 
-io.on('connection', socket=> {
-    console.log('a user connected');
-});
-
-http.listen(port, ()=> {
-    console.log(`listening on port ${port}`);
+http.listen(PORT, ()=> {
+    console.log(`listening on port ${PORT}`);
 });
